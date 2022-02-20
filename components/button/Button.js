76d97b133708353f283264fd2b6e6454ad1902a4
ash1895-web <aseeransari79@gmail.com>
2022-02-20 15:19:59 +0530

    typography and icons added

---
+++ components/button/Button.js
@@ -0,0 +1,11 @@
+import styles from './Button.module.css'
+
+const Button = () => {
+    return (
+        <div className = {styles.buttonContainer}>
+            apple eating disorder
+        </div>
+    )
+}
+
+export default Button
